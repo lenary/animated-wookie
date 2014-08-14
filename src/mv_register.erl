3d85c1a6708527e289b0dc79e1b10e3784626b39
Sam Elliott <sam@lenary.co.uk>
2014-08-14 19:55:42 +0100

    Copyright!

--- src/mv_register.erl
+++ src/mv_register.erl
@@ -1,3 +1,25 @@
+%% -------------------------------------------------------------------
+%%
+%% mv_register: Prototype Op-based Multi-Value Register
+%%
+%% Copyright (c) 2007-2014 Basho Technologies, Inc.  All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+
 -module(mv_register).
 -behaviour(po_crdt).
 
@@ -16,7 +38,7 @@ effect({wr, Val}, Ts, _Actor, Reg) ->
 eval(rd, Reg) ->
     [Val || {TsA, {wr, Val}} <- Reg,
             not lists:any(fun({TsB,_}) ->
-                                  vclock:lt(TsA, TsB)
+                                  vclock:lt(TsA, Ts)B
                           end, Reg)
     ].
 
--- src/po_crdt.erl
+++ src/po_crdt.erl
@@ -1,3 +1,25 @@
+%% -------------------------------------------------------------------
+%%
+%% po_crdt: Behaviour for PO-Log Op-Based CRDTs
+%%
+%% Copyright (c) 2007-2014 Basho Technologies, Inc.  All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+
 -module(po_crdt).
 
 -type po_log_crdt() :: term().
